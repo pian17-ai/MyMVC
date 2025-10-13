65c5b9d15a05af06ec7611b8e73aa26a424443f3
pian17-ai <alviancahyopambudi@gmail.com>
2025-10-13 23:15:00 +0700

    URL update

---
+++ app/controllers/About.php
@@ -0,0 +1,11 @@
+<?php
+
+class About {
+    public function index() {
+        echo "About/index";
+    }
+
+    public function aboutme() {
+        echo "Saya pian";
+    }
+}
--- app/core/App.php
+++ app/core/App.php
@@ -1,11 +1,35 @@
 <?php
 
 class App {
+    protected $controller = "Home";
+    protected $method = "index";
+    protected $params = [];
+
     public function __construct()
     {
         $url = $this->parseURL();
+        
+        // controller
+        if (!empty($url) && file_exists("../app/controllers/" . $url[0] . ".php")) {
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
 
-        var_dump($url);
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+        // method
+        if (!empty($url[1])) {
+            $this->method = $url[1];
+            unset($url[1]);
+        }
+
+        // params
+        if (!empty($url)) {
+            $this->params = array_values($url);
+        }
+
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
     public function parseURL() {
