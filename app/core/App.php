f3520b9a614f333eae0624cbd068c01f2a128dfe
pian17-ai <alviancahyopambudi@gmail.com>
2025-10-13 23:00:29 +0700

    parse URL

---
+++ app/core/App.php
@@ -0,0 +1,21 @@
+<?php
+
+class App {
+    public function __construct()
+    {
+        $url = $this->parseURL();
+
+        var_dump($url);
+    }
+
+    public function parseURL() {
+        if (isset($_GET['url'])) {
+            $url = $_GET['url'];
+            $url = rtrim($url, '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
+        return $url=[];
+    }
+}
