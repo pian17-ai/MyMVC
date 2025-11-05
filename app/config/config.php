e0b6da59d71d23233dcf13d6a4c437638232f031
pian17-ai <alviancahyopambudi@gmail.com>
2025-11-05 10:06:09 +0700

    update

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/MyMVC');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'pma');
+define('DB_PASS', 'pmapass');
+define('DB_NAME', 'db_mymvc');
